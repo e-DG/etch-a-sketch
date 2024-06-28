427806fb512fed561fe2a38e27bb82887ff1a888
e-DG <edudg95@hotmail.com>
2024-06-29 00:39:54 +0200

    Added change of color functionality with CSS (temporal)

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-const container = document.querySelector(".container");
+const container = document.querySelector("#container");
 
-for (let i = 0; i <= 32; i++) {
+ for (let i = 0; i <= 32; i++) {
     const div = document.createElement("div");
     div.setAttribute("id", "pixel");
     container.appendChild(div);
 }
+
+
