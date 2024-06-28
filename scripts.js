7f02360e552333433fc6a1a73164dc7836dcc8a7
e-DG <edudg95@hotmail.com>
2024-06-29 00:51:52 +0200

    Fixed syntax error in JS (257 divs); Added Styles to #container div; Added :hover to #pixel

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 
- for (let i = 0; i <= 32; i++) {
+ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.setAttribute("id", "pixel");
     container.appendChild(div);
